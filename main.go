31ebfaf3b92713267482644e15845d6103953d0d
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2024-02-11 07:54:11 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,18 @@ import (
 
 func main() {
 	// Create an instance of EngToKana
-	engToKana := NewEngToKana(true)
+	engToKana := NewEngToKana()
+	// Create an instance of KanjiToKana
+	kanjiToKana := NewKanjiToKana()
+	// Create an instance of GanaToKana
+	hiraganaToKana := NewHiraganaToKatakana()
+	// Create an instance of KanjiSplitter
+	kanjiSplitter := NewKanjiSplitter(
+		kanjiToKana.Convert,			// Kanji callback
+		hiraganaToKana.Convert,			// Gana & Kana callback
+		engToKana.TranscriptSentence,	// English callback
+		convertToJapanesePunctuation,	// Punctuation callback
+	)
 
 	// Listen to stdin indefinitely
 	reader := bufio.NewReader(os.Stdin)
@@ -19,7 +30,7 @@ func main() {
 		}
 
 		// Call convertString function with the accumulated line
-		result := engToKana.TranscriptSentence(line)
+		result := kanjiSplitter.SeparateAndProcess(line)
 
 		// Output the result
 		fmt.Print(result+"\n")
